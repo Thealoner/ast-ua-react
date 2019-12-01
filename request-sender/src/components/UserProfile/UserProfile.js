b9883b14bebfbda4c03d8cc88150d4805fcb4e76
Eduard Nikolaiev <e.nikolaiev@astoundcommerce.com>
2019-12-01 13:50:49 +0200

    Requests with cancellation

--- request-sender/src/components/UserProfile/UserProfile.js
+++ request-sender/src/components/UserProfile/UserProfile.js
@@ -9,30 +9,99 @@ export default class UserProfile extends React.Component {
         this.state = {
             user: {
                 name: 'Default Name'
-            }
+            },
+            isLoading: false
         };
+        this.userId = React.createRef();
+    }
+
+    source = axios.CancelToken.source();
+
+    getUserAsync = async () => {
+        try {
+            this.setState({
+                isLoading: true
+            });
+            
+            var res = await axios.get(apiPeopleUrl + this.userId.current.value + '/', {
+                cancelToken: this.source.token
+            });
+            console.log(res.data);
+            this.setState({
+                user: res.data,
+                isLoading: false
+            });
+        } catch (e) {
+            if (axios.isCancel(e)) {
+                console.log('Error: ', e.message); // => prints: Api is being canceled
+            } else {
+                this.setState({
+                    isLoading: false
+                });
+            }
+        }
     }
 
     getUser() {
-        axios.get(apiPeopleUrl + '1/')
-            .then((res) => {
+        this.setState({
+            isLoading: true
+        });
+
+        axios.get(apiPeopleUrl + this.userId.current.value + '/', {
+            cancelToken: this.source.token
+        })
+            .then(res => {
                 console.log(res.data);
                 this.setState({
-                    user: res.data
+                    user: res.data,
+                    isLoading: false
                 });
+            })
+            .catch(e => {
+                if (axios.isCancel(e)) {
+                    console.log('Error: ', e.message);
+                } else {
+                    this.setState({
+                        isLoading: false
+                    });
+                }
             });
     }
 
+    cancelAllRequests() {
+        this.source.cancel('Operation cancelled');
+        this.source = axios.CancelToken.source();
+        this.setState({
+            isLoading: false
+        });
+    }
+
     componentDidMount() {
-        this.getUser();
+        console.log('cDM');
+        this.getUserAsync();
+    }
+    
+    componentDidUpdate() {
+        console.log('cDU');
+    }
+    
+    componentWillUnmount() {
+        console.log('cWU');
+        this.cancelAllRequests();
     }
 
     render() {
+        const { user, isLoading } = this.state;
+
         return (
             <div>
-                This is UserProfile. {this.state.user.name}
-                <input type='text' defaultValue='1' />
+                This is UserProfile.
+                <input type='text' defaultValue='1' ref={this.userId} />
+                <button onClick={() => this.getUserAsync()}>Get User Async</button>
                 <button onClick={() => this.getUser()}>Get User</button>
+                <button onClick={() => this.cancelAllRequests()}>Cancel</button>
+                <div>{isLoading ? 'Loading...' : ''}</div>
+                <div>{user.name ? user.name : ''}</div>
             </div>
         );
     }
